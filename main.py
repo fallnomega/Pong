136e6676e7d19e59bccbca53ca4814563dbf47ee
fallnomega <fallnomega@gmail.com>
2023-02-09 09:28:11 -0800

    paddle  and movement setup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # paddles/slider
 # individual player score
 # ball
+import time
 import turtle
 import paddle
 from turtle import Screen
@@ -18,7 +19,20 @@ from turtle import Screen
 screen = turtle.Screen()
 screen.screensize(canvwidth=800, canvheight=600, bg="black")
 screen.title("Pong")
+screen.tracer(0)
+screen.listen()
 
-player1 = paddle.Paddle()
+
+player = paddle.Paddle()
+screen.onkey(player.move_up,"Up")
+screen.onkey(player.move_down,"Down")
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -3,14 +3,24 @@ from turtle import Turtle
 
 class Paddle:
     def __init__(self):
-        paddle  = self.add_paddle()
+        self.paddle  = self.add_paddle()
 
     def add_paddle(self):
         new_paddle = turtle.Turtle()
         new_paddle.setheading(90)
-        new_paddle.setx(350)
+        new_paddle.goto(350,0)
         new_paddle.shapesize(stretch_wid=1,stretch_len=5)
         new_paddle.shape("square")
         new_paddle.color("white")
+        new_paddle.penup()
+        return new_paddle
+
+    def move_up(self):
+        self.paddle.forward(20)
+
+    def move_down(self):
+        self.paddle.backward(20)
+
+
 
 
