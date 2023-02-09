22fe23f714dc83f02b8dce1a9824c8463e7144e7
fallnomega <fallnomega@gmail.com>
2023-02-09 09:32:58 -0800

    paddle  and movement setup

--- paddle.py
+++ paddle.py
@@ -8,19 +8,20 @@ class Paddle:
     def add_paddle(self):
         new_paddle = turtle.Turtle()
         new_paddle.setheading(90)
+        new_paddle.penup()
         new_paddle.goto(350,0)
         new_paddle.shapesize(stretch_wid=1,stretch_len=5)
         new_paddle.shape("square")
         new_paddle.color("white")
-        new_paddle.penup()
         return new_paddle
 
     def move_up(self):
-        self.paddle.forward(20)
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(),new_y)
 
     def move_down(self):
-        self.paddle.backward(20)
-
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
 
 
 
