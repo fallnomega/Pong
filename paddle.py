246c56ee4eb22afe7b7e8fb54a8f5155b1477960
fallnomega <fallnomega@gmail.com>
2023-02-09 09:44:07 -0800

    paddle class cleanup

--- paddle.py
+++ paddle.py
@@ -2,24 +2,24 @@ import turtle
 from turtle import Turtle
 
 
-class Paddle():
+class Paddle(Turtle):
     def __init__(self, x_pos):
-        self.paddle = self.add_paddle(x_pos)
+        super().__init__()
+        self = self.add_paddle(x_pos)
 
     def add_paddle(self, x_position):
-        new_paddle = turtle.Turtle()
-        new_paddle.setheading(90)
-        new_paddle.penup()
-        new_paddle.goto(x_position, 0)
-        new_paddle.shapesize(stretch_wid=1, stretch_len=5)
-        new_paddle.shape("square")
-        new_paddle.color("white")
-        return new_paddle
+        self.setheading(90)
+        self.penup()
+        self.goto(x_position, 0)
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.shape("square")
+        self.color("white")
+        return self
 
     def move_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
