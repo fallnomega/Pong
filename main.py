127c9c5e3b6f34d27d12a906862ed96075247f7e
fallnomega <fallnomega@gmail.com>
2023-02-09 12:16:25 -0800

    pong

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-# classes in pong
-# paddles/slider
-# individual player score
-# ball
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 import time
-import turtle
-import paddle
-import ball
-import ball
-from turtle import Screen
 
-# TODO list
-
-# 4 Create the ball and make it move
-# 5 Detect collision with wall and bounce
-# 6 Detect collision with paddle
-# 7 Detect when paddle misses
-# 8 Keep Score
-
-screen = turtle.Screen()
-screen.screensize(canvwidth=800, canvheight=600, bg="black")
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
 screen.listen()
-
-player1 = paddle.Paddle(-350)
-player2 = paddle.Paddle(350)
-ball = ball.Ball()
-screen.onkey(player1.move_up, "Up")
-screen.onkey(player1.move_down, "Down")
-
-screen.onkey(player2.move_up, "w")
-screen.onkey(player2.move_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    # time.sleep(.1)
-    ball.ball_move()
+    ball.move()
+
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect L paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -2,23 +2,19 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, x_pos):
-        super().__init__()
-        self = self.add_paddle(x_pos)
 
-    def add_paddle(self, x_position):
-        self.setheading(90)
-        self.penup()
-        self.goto(x_position, 0)
-        self.shapesize(stretch_wid=1, stretch_len=5)
+    def __init__(self, position):
+        super().__init__()
         self.shape("square")
         self.color("white")
-        return self
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
-    def move_up(self):
+    def go_up(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
-    def move_down(self):
+    def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
